7d06be34849bc7e1d2d0f226aaed2a65c304bddb
Nilugette <nilugette@yahoo.fr>
2021-02-28 10:36:15 +0100

    Expanding the accordion

--- src/components/Accordion.js
+++ src/components/Accordion.js
@@ -1,14 +1,29 @@
-import React from 'react'
+import React, { useState } from 'react'
+
+
 
 const Accordion = ( { items }) => {
-    const rederedItems = items.map(item => {
+    // The array distructuring [activeIndex, setActiveIndex] is identical to object distructuring, it's a short cut to get access to some elements inside an array
+    // Whenever we call useState we get back an array with 2 elements that we 
+    // useState is a Hook : it gives access to the state system inside a functinal component
+    const [activeIndex, setActiveIndex] = useState(null)
+
+    const onTitleClick = (index) =>  {
+        setActiveIndex(index)
+    }
+    const rederedItems = items.map((item, index)=> {
+        const active = index === activeIndex ? 'active': ''
+
         return (
             <React.Fragment key={item.title}>
-                <div className="title active"> 
+                <div 
+                  className= {`title ${active}`}
+                  onClick={() => onTitleClick(index)}
+                > 
                     <i className="dropdown icon"></i>
                     {item.title}
                 </div>
-                <div className="title active">
+                <div className={`content ${active}`}>
                     <p>{item.content}</p>
                 </div>
             </React.Fragment>
